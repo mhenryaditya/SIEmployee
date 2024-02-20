33739505c7f1576ed84cd416e7eb8a8d250e57ca
itsmerahmat <rahmatgamming1@gmail.com>
2024-02-20 17:07:24 +0800

    Feat<Project>: Nambah Pesan

--- app/Views/project.php
+++ app/Views/project.php
@@ -4,6 +4,6 @@
 
 <?= $this->section('section') ?>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem vero pariatur velit dicta a, odio, saepe
-    delectus maxime, dignissimos sit libero. Quaerat, quam veritatis necessitatibus dolorum quod odio tempora voluptas?
+    delectus maxime, dignissimos sit libero. Quaerat, quam veritatis necessitatibus dolorum quod odio tempora voluptas? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quasi, libero eum voluptates saepe dicta deserunt quibusdam, hic omnis temporibus quod obcaecati sapiente reprehenderit ea. Magnam reprehenderit totam deserunt adipisci?
 </p>
 <?= $this->endSection() ?>
